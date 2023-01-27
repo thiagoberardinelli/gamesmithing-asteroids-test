5658da1ba1b6e77dda3c0817bbcd21676c10267b
Thiago Berardinelli <thberardinelli@gmail.com>
2023-01-27 19:23:51 -0300

    Added bullet prefab and ship fire behaviour
    + Added fire method to the ship class
    + Created bullet prefab
    + Created bullet class with its behaviour

---
+++ Assets/_Project/Scripts/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace _Project.Scripts
+{
+    public class Bullet : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody2D rgbd;
+        [SerializeField] private float speed;
+        
+        private void Start() => rgbd.AddRelativeForce(Vector3.up * speed, ForceMode2D.Force);
+
+        private void Update()
+        {
+            if (!IsOffScreen()) return;
+            Destroy(gameObject, 0.1F);
+        }
+
+        private bool IsOffScreen()
+        {
+            Vector3 transformViewportPoint = Camera.main.WorldToViewportPoint(transform.position);
+
+            return transformViewportPoint.x > 1 || transformViewportPoint.x < 0 ||
+                   transformViewportPoint.y > 1 || transformViewportPoint.y < 0;
+        }
+    }
+}
--- Assets/_Project/Scripts/Ship.cs
+++ Assets/_Project/Scripts/Ship.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 namespace _Project.Scripts
@@ -8,13 +7,20 @@ namespace _Project.Scripts
         [SerializeField] private Rigidbody2D rgbd;
         [SerializeField] private float thrust;
         [SerializeField] private float rotationForce;
-        [SerializeField] private Transform laserSpawnTransform;
-
-        private const float MaxLimitSpeed = 5F;
+        [SerializeField] private Transform bulletSpawnPoint;
+        [SerializeField] private GameObject bulletPrefab;
         
+        private readonly float _maxLimitSpeed = 5F;
+        private readonly float _fireRate = 0.2F;
+        private float _nextBullet;
+
         private void Update()
         {
+            if (!Input.GetKeyDown(KeyCode.Space) || Time.time < _nextBullet) return;
+
+            _nextBullet = Time.time + _fireRate;
             
+            Fire();
         }
 
         private void FixedUpdate()
@@ -30,8 +36,8 @@ namespace _Project.Scripts
         {
             rgbd.AddRelativeForce(Vector3.up * thrust * verticalInput, ForceMode2D.Force);
 
-            Vector2 clampedVelocity = new Vector2(Mathf.Clamp(rgbd.velocity.x, -MaxLimitSpeed, MaxLimitSpeed),
-                Mathf.Clamp(rgbd.velocity.y, -MaxLimitSpeed, MaxLimitSpeed));
+            Vector2 clampedVelocity = new Vector2(Mathf.Clamp(rgbd.velocity.x, -_maxLimitSpeed, _maxLimitSpeed),
+                Mathf.Clamp(rgbd.velocity.y, -_maxLimitSpeed, _maxLimitSpeed));
 
             rgbd.velocity = clampedVelocity;
         }
@@ -39,6 +45,11 @@ namespace _Project.Scripts
         private void Rotate(float horizontalInput)
         {
             transform.Rotate(0, 0, -horizontalInput * rotationForce * Time.deltaTime);
+        }
+
+        private void Fire()
+        {
+            Instantiate(bulletPrefab, bulletSpawnPoint.position, transform.rotation);
         }
     }
 }
